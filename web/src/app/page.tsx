cdb48ea0a292854b1a408d14a1ecea65fb143b65
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2023-07-01 01:05:46 -0300

    update 1

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,56 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+
+import nlwLogo from '../assets/nlw-spacetime-logo.svg'
+
 export default function Home() {
   return (
-    <h1 className="m-10 h-20 w-20 rounded bg-red-500 text-2xl font-bold uppercase">
-      Hello World
-    </h1>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+        {/* Stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2  bg-stripes"></div>
+
+        <a
+          href=""
+          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+
+          <p className="max-w-[140px] text-sm leading-snug">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div>
+          <Image src={nlwLogo} alt="NLW Spacetime" />
+          <div>
+            <h1>Sua cápsula do tempo</h1>
+            <p>
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Right */}
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[368px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{' '}
+            <a className="underline hover:text-gray-50" href="">
+              criar agora
+            </a>
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
